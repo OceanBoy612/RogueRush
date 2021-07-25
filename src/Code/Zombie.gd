9d7bba16b3ea32ab07dbbd9165ee6bb4cd52cc3a
Yonatan Attali <yonatan.attali@gmail.com>
2021-07-25 03:48:03 -0400

    Created zombie who moves back an forth on a platform

---
+++ src/Code/Zombie.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+
+
+var move_dir = Vector2(1,0)
+var move_speed = 100
+var gravity = Vector2(0,100)
+var timer = 0
+
+
+func _physics_process(delta):
+	
+	move_and_slide(gravity + move_dir * move_speed * Global.time_scale)
+	timer += delta
+	
+	if timer > 0.4:
+		var l = int($left.is_colliding())
+		var r = int($right.is_colliding())
+		var rs = int($right_side.is_colliding())
+		var ls = int($left_side.is_colliding())
+		
+		var over_ledge = l ^ r
+		var against_wall = (l & r) & (rs ^ ls)
+		
+		if over_ledge or against_wall:
+			move_dir *= -1
+			timer = 0
